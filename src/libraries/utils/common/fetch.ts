25a8af64adcbdea6eb632854be94caba35474382
Jan Leigh Muñoz <janleigh@proton.me>
2023-08-12 09:55:59 +0800

    chore: update node-fetch to v3

--- src/libraries/utils/common/fetch.ts
+++ src/libraries/utils/common/fetch.ts
@@ -1,21 +1,22 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import fetch from "node-fetch";
-
 const API_URL = process.env.API_URL ?? "http://localhost:8080";
 
 // Closed source ;) No stealing
 export const pingServer = async () => {
-	return await fetch(API_URL)
-		.then(() => {
-			return true;
-		})
-		.catch(() => {
-			return false;
-		});
+	return await import("node-fetch").then(({ default: fetch }) =>
+		fetch(API_URL)
+			.then(() => {
+				return true;
+			})
+			.catch(() => {
+				return false;
+			})
+	);
 };
 
 export const fetchResponseFromAI = async (message: string, uid: string) => {
-	const response = await fetch(`${API_URL}/response?message=${message}&userId=${encodeURIComponent(uid)}`);
+	const response = await import("node-fetch").then(({ default: fetch }) =>
+		fetch(`${API_URL}/response?message=${message}&userId=${encodeURIComponent(uid)}`)
+	);
 	if (response instanceof Error) {
 		return response;
 	}
